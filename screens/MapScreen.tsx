4d8ebc86cb587086b5161da358d890194821cfba
miguelin1 <mcruz127@ymail.com>
2026-08-26 11:31:49 -0500

    Hide Search This Area button when store is selected

--- screens/MapScreen.tsx
+++ screens/MapScreen.tsx
@@ -255,7 +255,7 @@ export default function MapScreen() {
         </View>
       </View>
 
-      {showSearchButton && (
+      {showSearchButton && !selected && (
         <TouchableOpacity 
           style={styles.searchBtn}
           onPress={handleSearchThisArea}
