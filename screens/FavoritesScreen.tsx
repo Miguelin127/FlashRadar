f1434281f180bba9c1b172f6b22d026a517c3bda
miguelin1 <mcruz127@ymail.com>
2026-09-09 16:42:13 -0500

    Add debug log to onViewDeal

--- screens/FavoritesScreen.tsx
+++ screens/FavoritesScreen.tsx
@@ -249,7 +249,7 @@ export default function FavoritesScreen() {
                 onToggleSave={async () => {}}
                 distance={userLocation && item.latitude ? distanceMiles(userLocation, { latitude: item.latitude, longitude: item.longitude || 0 }) : null}
                 onOpenMaps={() => openMaps(item)}
-                onViewDeal={() => { (navigation as any).navigate("DealDetail", { deal: item }); }}
+                onViewDeal={() => { console.log("onViewDeal called"); (navigation as any).navigate("DealDetail", { deal: item }); }}
                 isLocked={isLocked}
                 isPulsing={isLive}
                 theme={theme}
